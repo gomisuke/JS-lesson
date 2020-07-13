a8bd43269802ed597067f7fce5a1f3b1a0bfa913
五味佳佑 <gomikeisuke@MacBook-Pro-4.local>
2020-07-13 15:45:15 +0900

    [Finish]lesson8

---
+++ SlideShow-Lesson/js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+    transition : 'fade',
+    speed : 1000,
+    easing : 'ease0utQuart',
+    navType : 'block',
+    childrenElementType : 'div',
+    arrows : true,
+    autoPlay : false,
+    autoPlayDuration : 5000,
+    keyboard0nAlways : true,
+    hidePrevious : false
+});
