921c2ca01a1e9df1a0c6bc78c288f248a000d9b2
五味佳佑 <gomikeisuke@MacBook-Pro-4.local>
2020-07-13 14:51:19 +0900

    [Finish]lesson7

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -19,13 +19,20 @@ $(function(){
     });
 });
 */
-
+/*
 $(function(){
     $('.box1').on('click', function(){
         $('.box1').addClass('box1-ext');
     });
     $('.box1').mouseout(function(){
         $('.box1').removeClass('box1-ext');
+    });
+});
+*/
+
+$(function(){
+    $('button').on('click', function(){
+        $('ul').children().css('color','red');
     });
 });
 
