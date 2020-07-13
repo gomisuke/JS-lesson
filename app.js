538459cbc11d2e13280eb64d98d90d3d8b2f7869
五味佳佑 <gomikeisuke@MacBook-Pro-4.local>
2020-07-13 14:20:29 +0900

    [Finish]lesson5

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var hello = "Hello World";
 alert(hello);
-
+/*
 var max = 100;
 var num = 1;
 var count = 0;
@@ -25,3 +25,80 @@ alert(1+2);
 alert(1*2);
 alert(1/2);
 alert(1-2);
+
+var alertString;
+alertString = addString("WebCamp");
+
+alert(alertString);
+
+function addString(strA){
+    var addStr = "Hello" + strA;
+    return addStr;
+}
+
+var promptStr = prompt('何か好きな文字を入れてください。');
+
+alert(promptStr);
+*/
+
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while((user_hand != "グー")&&(user_hand != "チョキ")&&(user_hand != "パー")&&(user_hand != null)){
+    alert('グー・チョキ・パーのいずれかを入力してください')
+    user_hand = prompt('じゃんけんの手をグー・チョキ・パーから選んでください');
+}
+
+var js_hand = getJShand();
+
+var judge = winLose(user_hand, js_hand);
+
+if(user_hand != null){
+    alert('あなたの選んだては' + user_hand + 'です。\nJavaScriptの選んだては' + js_hand + 'です。\n結果は' + judge + 'です');
+}else{
+    alert('またチャレンジしてね');
+}
+
+function getJShand(){
+    var js_hand_num = Math.floor( Math.random() * 3 );
+
+    if(js_hand_num == 0){
+        js_hand = "グー";
+    }else if(js_hand_num == 1){
+        js_hand = 'チョキ';
+    }else if(js_hand_num =- 2){
+        js_hand = 'パー';
+    }
+
+    return js_hand;
+}
+
+function winLose(user, js){
+    var winLoseStr;
+
+    if(user == "グー"){
+        if(js == "グー"){
+            winLoseStr = "あいこ";
+        }else if(js == "チョキ"){
+            winLoseStr = "勝ち";
+        }else if(js == "パー"){
+            winLoseStr = "負け";
+        }
+    }else if(user == "チョキ"){
+        if(js == "グー"){
+            winLoseStr = "負け";
+        }else if(js == "チョキ"){
+            winLoseStr = "あいこ";   
+        }else if(js == "パー"){
+            winLoseStr = "勝ち";
+        }
+    }else if(user == "パー"){
+        if(js == "グー"){
+            winLoseStr = "勝ち";
+        }else if(js == "チョキ"){
+            winLoseStr = "負け";
+        }else if(js == "パー"){
+            winLoseStr = "あいこ";
+        }
+    }
+
+    return winLoseStr;
+}
